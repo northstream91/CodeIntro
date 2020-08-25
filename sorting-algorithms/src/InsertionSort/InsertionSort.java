7e4e9be26587f3a9eda77e4a952b97f5d5e6ee2e
Jonas Nordström <jonas_nordstrom@live.se>
2020-08-25 12:53:01 +0200

    Added insertion sort

---
+++ sorting-algorithms/src/InsertionSort/InsertionSort.java
@@ -0,0 +1,20 @@
+package InsertionSort;
+
+public class InsertionSort {
+
+    public static int[] sort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+            for (int j = i; j > 0 && list[j - 1] > list[j]; j--) {
+                swap(list, j, j - 1);
+            }
+        }
+        return list;
+    }
+
+    private static void swap(int[] list, int indexA, int indexB) {
+        int value = list[indexA];
+        list[indexA] = list[indexB];
+        list[indexB] = value;
+    }
+
+}

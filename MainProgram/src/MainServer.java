362f9247bbb8832d2da0bb184b9397a9f3776ea4
Jonas Nordström <jonas_nordstrom@live.se>
2020-08-25 09:47:13 +0200

    Very first commit

---
+++ MainProgram/src/MainServer.java
@@ -0,0 +1,10 @@
+public class MainServer {
+    public static void main(String[] args) {
+        System.out.println("Program is started.");
+
+        System.out.println();
+
+
+        System.out.println("Program has ended.");
+    }
+}
